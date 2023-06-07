5772569306db731db5d88168bb3ad9b9bfcb8054
S B Chand Basha <124661910+chandbasha304@users.noreply.github.com>
2023-06-07 11:21:03 +0530

    Update ConstantValues.java

--- ConstantValues.java
+++ ConstantValues.java
@@ -2,9 +2,9 @@ package travels;
 
 public class ConstantValues 
 {
-public static final String dbUSer="root";
-public static final String dbpassword="root";
-public static final String dburl="jdbc:mysql://localhost:3306/bata";
+public static final String dbUSer="";
+public static final String dbpassword="";
+public static final String dburl="jdbc:mysql:///bata";
 public static final String logo="C:\\Downloads\\logo.txt";
 public static final int count=5;
 public static final double gst=0.05;
