2f1b9fdaeb85fc0c0941843c9e002a8ea2b41aa6
Chand_Basha <schandbasha304@gmail.com>
2024-02-16 11:01:02 +0530

    New Java Class Creating

--- JustCreat.java
+++ JustCreat.java
@@ -6,4 +6,12 @@ public class JustCreat {
     public static void main(String[] args) {
         System.out.println("Welcome to Github");
     }
+
+
+    public String newAlertMessage()
+    {
+
+        return "Happy New Year";
+    }
+
 }
